6aaa7c358be4f0c8b66a59c22f3b625fe6df0c65
jmcord <jcorderodz@hotmail.com>
2024-02-08 13:39:41 +0100

    c

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 
-from functions import alta_prestamista, alta_cliente, depositar_garantia, solicitar_prestamo, aprobar_prestamo, reembolsar_prestamo, obtener_detalle_de_prestamo, obtener_prestamos_por_prestatario
+from functions import alta_prestamista, alta_cliente, depositar_garantia, solicitar_prestamo, aprobar_prestamo, reembolsar_prestamo, obtener_detalle_de_prestamo, obtener_prestamos_por_prestatario, liquidar_garantia
 from env import *
 from web3 import Web3
 
@@ -105,6 +105,20 @@ def main():
             #PRUEBAS: 1-reembolsar dos veces el mismo id
             #PRUEBAS: 2-utilizar un id incorrecto
             
+        elif opcion == "7":
+            prestamo_id = int(input('Introduce el id del prestamo: '))
+            prestatario_address = input('Introduce el address del cliente: ')
+            prestamista_address = input('Introduce el address del prestamista: ')
+            #Check if the input address is correct
+            #PRUEBA: verificamos addresses
+            if not web3.is_checksum_address(prestatario_address):
+                print('Error: La cuenta prestatario no es válida')
+                exit()
+
+            liquidar_garantia(prestamo_id, prestatario_address, abi_contrato, cliente_private_key)
+            #PRUEBAS: 1-reembolsar dos veces el mismo id
+            #PRUEBAS: 2-utilizar un id incorrecto
+            
         elif opcion == "8":
             prestatario_address = input('Introduce el address del cliente: ')
             #Check if the input address is correct
@@ -133,22 +147,7 @@ def main():
             #PRUEBAS: 1-introducir un id inexistente
             #PRUEBAS: 2-introducir un address inexistente
 
-    
-            
-        elif opcion == "9":
-            prestamo_id = int(input('Introduce el id del prestamo: '))
-            prestatario_address = input('Introduce el address del cliente: ')
-            #Check if the input address is correct
-            #PRUEBA: verificamos addresses
-            if not web3.is_checksum_address(prestatario_address):
-                print('Error: La cuenta prestatario no es válida')
-                exit()
-
-            prestamo = obtener_detalle_de_prestamo(prestatario_address, prestamo_id, abi_contrato)
-            print(prestamo)
-            #PRUEBAS: 1-introducir un id inexistente
-            #PRUEBAS: 2-introducir un address inexistente
-
+  
         elif opcion == "0":
             print("Saliendo del programa...")
             break
